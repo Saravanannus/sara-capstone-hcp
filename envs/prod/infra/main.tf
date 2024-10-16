5fb4fe03401c3db03bc5753e3bdfbfc2f1fe7af2
Saravanan.B <saravanannus@gmail.com>
2024-10-17 01:11:40 +0800

    update main.tf

--- envs/prod/infra/main.tf
+++ envs/prod/infra/main.tf
@@ -1,5 +1,6 @@
 locals {
-  prefix = "ce6-grp1-prod"  # Append environment to prefix
+  prefix = "ce6-grp1"  # Append environment to prefix
+  environment = "prod"
 }
 
 data "aws_caller_identity" "current" {}
@@ -26,7 +27,7 @@ module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "~> 5.9.0"
 
-  cluster_name = "${local.prefix}-ecs"
+  cluster_name = "${local.prefix}-ecs-${local.environment}"
 
   fargate_capacity_providers = {
     FARGATE = {
@@ -36,36 +37,36 @@ module "ecs" {
     }
   }
 
-  services = {
-    web-prod = { #task def and service name -> #Change
-      cpu    = 512
-      memory = 1024
-      # Container definition(s)
-      container_definitions = {
-        web-prod-ecs-container = { #container name -> Change
-          essential = true
-          image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
-          port_mappings = [
-            {
-              containerPort = 8080
-              protocol      = "tcp"
-            }
-          ]
-        }
-      }
-      assign_public_ip                   = true
-      deployment_minimum_healthy_percent = 100
-      subnet_ids                         = flatten(data.aws_subnets.public.ids)
-      security_group_ids                 = [module.ecs_sg.security_group_id]
-    }
-  }
+  # services = {
+  #   web = { #task def and service name -> #Change
+  #     cpu    = 512
+  #     memory = 1024
+  #     # Container definition(s)
+  #     container_definitions = {
+  #       web-ecs-container = { #container name -> Change
+  #         essential = true
+  #         image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
+  #         port_mappings = [
+  #           {
+  #             containerPort = 8080
+  #             protocol      = "tcp"
+  #           }
+  #         ]
+  #       }
+  #     }
+  #     assign_public_ip                   = true
+  #     deployment_minimum_healthy_percent = 100
+  #     subnet_ids                         = flatten(data.aws_subnets.public.ids)
+  #     security_group_ids                 = [module.ecs_sg.security_group_id]
+  #   }
+  # }
 }
 
 module "ecs_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 5.1.0"
 
-  name        = "${local.prefix}-ecs-sg"
+  name        = "${local.prefix}-ecs-sg-${local.environment}"
   description = "Security group for ecs"
   vpc_id      = data.aws_vpc.default.id
 
